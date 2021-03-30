a987d215f38b3212d5de77d4c7c18db7e80aec3d
Oscar <o.ceretti@reply.it>
2021-03-30 23:36:30 +0200

    add some tests on NBAManager

--- NBA Teams/NBA TeamsTests/NBA_TeamsTests.swift
+++ NBA Teams/NBA TeamsTests/NBA_TeamsTests.swift
@@ -10,10 +10,124 @@ import XCTest
 
 class NBA_TeamsTests: XCTestCase {
 
+    var sut: NBAManager? = NBAManager()
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        sut = nil
     }
+    
+    func test_getTeamsList_ShuldBenotNil() {
+        var getTeamsOutput: Teams?
+        var err: Error?
+        sut = NBAManager()
+        
+        let expectation = XCTestExpectation(description: "Expect to get not nil")
+        //When
+        sut?.getTeamList() {
+            debugPrint("start")
+        } completion: { (result) in
+            switch result {
+            
+            case .success(let data):
+                getTeamsOutput = data
+                expectation.fulfill()
+            case .failure(let error):
+                err = error
+            }
+        } onComplete: {
+            
+        }
+        
+        wait(for: [expectation], timeout: 5.0)
 
+        //Then
+        XCTAssertNotNil(getTeamsOutput)
+    }
+    
+    func test_getTeamsList_ShuldBeNil() {
+        var getTeamsOutput: Teams?
+        var err: Error?
+        sut = NBAManager()
+        
+        let expectation = XCTestExpectation(description: "Expect to get not nil")
+        //When
+        sut?.getTeamList() {
+            debugPrint("start")
+        } completion: { (result) in
+            switch result {
+            
+            case .success(let data):
+                getTeamsOutput = data
+                expectation.fulfill()
+            case .failure(let error):
+                err = error
+            }
+        } onComplete: {
+            
+        }
+        
+        wait(for: [expectation], timeout: 5.0)
+
+        //Then
+        XCTAssertNotNil(getTeamsOutput)
+    }
+    
+    
+    func test_getPlayerList_ShuldBeNotNil() {
+        var getPlayerOutput: PlayersContainer?
+        var err: Error?
+        sut = NBAManager()
+        
+        let expectation = XCTestExpectation(description: "Expect to get not nil")
+        //When
+        sut?.getPLayerList(page: 1) {
+            debugPrint("start")
+        } completion: { (result) in
+            switch result {
+            
+            case .success(let data):
+                getPlayerOutput = data
+                expectation.fulfill()
+            case .failure(let error):
+                err = error
+            }
+        } onComplete: {
+            
+        }
+        
+        wait(for: [expectation], timeout: 5.0)
+
+        //Then
+        XCTAssertNotNil(getPlayerOutput)
+    }
+    
+    func test_getSinglePlayer_ShuldBeNotNil() {
+        var getPlayerOutput: Player?
+        var err: Error?
+       
+        sut = NBAManager()
+        
+        let expectation = XCTestExpectation(description: "Expect to get not nil")
+        //When
+        sut?.getPlayer(id: 1) {
+            debugPrint("start")
+        } completion: { (result) in
+            switch result {
+            case .success(let data):
+                getPlayerOutput = data
+                expectation.fulfill()
+            case .failure(let error):
+                err = error
+            }
+        } onComplete: {
+            
+        }
+        
+        wait(for: [expectation], timeout: 5.0)
+        //Then
+        XCTAssertNotNil(getPlayerOutput)
+    }
+    
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
@@ -29,5 +143,10 @@ class NBA_TeamsTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
+    
+    func createSut() -> NBAManager {
+        return NBAManager()
+    }
 
 }
+
